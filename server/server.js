e3215090051e3317325fa2f9be4a913add2d7965
Jan Gross <jan.gross.cze@gmail.com>
2023-08-15 13:43:19 +0200

    basic scraping code added to server.js

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const mysql = require('mysql');
+const cors = require('cors');
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+app.use(cors());    
+app.use(express.json());
+
+//create connection to database
+
+
+//scraping data from website
+const url = 'https://www.gymio.com/'
+
+request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const $ = cheerio.load(body);
+  
+      // Získání všech zakomentovaných informací v sekci "head"
+      $('head').contents().each((index, element) => {
+        if (element.type === 'comment') {
+          console.log(element.data.trim());
+        }
+      });
+    } else {
+      console.error('Chyba při požadavku na stránku:', error);
+    }
+  });
+
+
+app.listen(port, () => console.log(`Server running on PORT ${port} 🔥🔥🔥`))
